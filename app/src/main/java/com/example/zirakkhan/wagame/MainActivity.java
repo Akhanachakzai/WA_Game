190044f90084e6a2c4a5836e93478d7b1b819a12
Akhanachakzai <akhanachakzai1@gmail.com>
2015-11-22 03:45:07 +0500

    Initial commit

---
+++ app/src/main/java/com/example/zirakkhan/wagame/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.zirakkhan.wagame;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Button;
+import android.gesture.Gesture;
+import android.media.AudioTrack;
+import android.support.v4.view.GestureDetectorCompat;
+
+
+public class MainActivity extends AppCompatActivity {
+    TextView Start_text;
+    Button Play;
+    Button Set;
+    public MediaPlayer intro_Sound;
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        intro_Sound.release();
+    }
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Play = (Button)findViewById(R.id.play);
+        Set = (Button)findViewById(R.id.settings);
+        Start_text = (TextView)findViewById(R.id.start_text);
+
+
+         intro_Sound = MediaPlayer.create(MainActivity.this,R.raw.intro_sound);
+        intro_Sound.start();
+        intro_Sound.setVolume(100,100);
+
+
+
+    }
+
+
+
+}
